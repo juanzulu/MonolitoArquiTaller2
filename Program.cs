9d1f51124f8544b1a0e0eda165a16ec614ec6072
Juan Miguel Zuluaga Suárez <junzulu28@gmail.com>
2025-04-18 14:30:30 -0500

    First Conection DB

--- Program.cs
+++ Program.cs
@@ -1,7 +1,12 @@
+﻿using MonolitoTaller.Models.DAO;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<EstudianteDAO>();
+builder.Services.AddScoped<AsignaturaDAO>();
+builder.Services.AddScoped<NotaDAO>();// << Asegúrate de registrar tu DAO aquí
 
 var app = builder.Build();
 
@@ -9,8 +14,11 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+else
+{
+    app.UseDeveloperExceptionPage(); // << agrega esto para ver el error real
 }
 
 app.UseHttpsRedirection();
